d31e9907f512b6b6a65392a45f951887c3b4ba58
HYOJIN PARK <hyojinpark.world@gmail.com>
2019-06-28 00:21:31 +0900

    추가

--- src/main/java/com/jpa/demo/JpaApplication.java
+++ src/main/java/com/jpa/demo/JpaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com/jpa"})
+@EntityScan(basePackages = {"com.jpa"})
 @EnableJpaRepositories(basePackages = {"com.jpa"})
 public class JpaApplication {
 
